a8c2d7767d9789188ff6f6fc7db79a1c7a30e47d
Anfifo <afonseca-1995@hotmail.com>
2016-10-28 22:15:28 +0100

    beautifying + organization and comments

--- exercicio2/i-banco.c
+++ exercicio2/i-banco.c
@@ -4,421 +4,67 @@
  * Andre' Fonseca 84698
  * Isabel Dias 84726
  * 
- * added:
- * 		Comandos: "sair", "sair agora" and " simular"
- * 		funcao: apanhaSignalSIGUSR1()
- * 		Multi Processos para exercutar o Simular
- * 		Comentarios
  * * * * * * * * * * * * *
-// Projeto SO - exercicio 1, version 1
+// Projeto SO - exercicio 2, version 1
 // Sistemas Operativos, DEI/IST/ULisboa 2016-17
 */
 
-#include "commandlinereader.h"
-#include "contas.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <semaphore.h>
-#include <pthread.h>
+#include "inputProcessing.h"
 
-/* commands */
-#define COMANDO_DEBITAR "debitar"
-#define COMANDO_CREDITAR "creditar"
-#define COMANDO_LER_SALDO "lerSaldo"
-#define COMANDO_SIMULAR "simular"
-#define COMANDO_SAIR "sair"
-#define COMANDO_SAIR_AGORA "agora"
+/**
+ * Initializes all necessary resources for i-banco to function with 
+ * all features and calls the functions for it to work
+ */
 
-/* operations related to the commands */
-#define OPERACAO_DEBITAR 1337
-#define OPERACAO_CREDITAR 666
-#define OPERACAO_LER_SALDO 007
-#define OPERACAO_SIMULAR 2319
-#define OPERACAO_SAIR 777
-
-
-#define MAXARGS 3
-#define BUFFER_SIZE 100
-#define NR_MAX_PROCESSOS 20
-#define NUM_TRABALHADORAS 3
-#define CMD_BUFFER_DIM (NUM_TRABALHADORAS * 2)
-
-
-typedef struct {
-	int operacao;
-	int idConta;
-	int valor;
-}comando_t;
-
-
-
-
-
-/* functions declarations */
-
-/* function that processes signal SIGUSR1 */
-void apanhaSinalSIGUSR1();
-
-void processCommand(comando_t dualshock);
-
-void addToBuffer(comando_t dualshock);
-
-void* readBuffer();
-
-void initAccountMutexes();
-
-void processCommand(comando_t dualshock);
-
-void processInput();
-
-
-
-/*-- global variables --*/
-
-/* flag changed by signal SIGUSR1 used in child process */
-
-extern int flag;
-
-comando_t cmd_buffer[CMD_BUFFER_DIM];
-
-pthread_t thread_pool[NUM_TRABALHADORAS];
-
-pthread_mutex_t reading_mutex;
-pthread_mutex_t account_mutexes[NUM_CONTAS];
-sem_t writer_sem;
-sem_t reader_sem;
-
-int accounts_in_use[NUM_TRABALHADORAS];
-int buff_write_idx;
-int buff_read_idx;
-
-int flag_sair;
-
-
-
-
-
-/*-- functions --*/
-
-void addToBuffer(comando_t dualshock){
-	
-	sem_wait(&writer_sem);
-
-	cmd_buffer[buff_write_idx] = dualshock;
-	buff_write_idx = (buff_write_idx + 1) % CMD_BUFFER_DIM;
-	
-	sem_post(&reader_sem);
-}
-
-
-void* readBuffer(){
-	comando_t dualshock;
-
-	while (!flag_sair){
-		sem_wait(&reader_sem);
-		
-		pthread_mutex_lock(&reading_mutex);
-		
-		dualshock = cmd_buffer[buff_read_idx];
-		buff_read_idx = (buff_read_idx + 1) % CMD_BUFFER_DIM;
-		
-		pthread_mutex_unlock(&reading_mutex);
-		
-		sem_post(&writer_sem);
-		
-		processCommand(dualshock);
-	}
-	
-	pthread_exit(NULL);
-	printf("terminou um thread\n");
-	
-	return NULL;
-}
-
-
-
-void initAccountMutexes() {
-	int i;
-	for(i = 0; i < NUM_CONTAS; i++)
-		pthread_mutex_init(account_mutexes + i, NULL);
-}
-
-
-
-void processCommand(comando_t dualshock){
-
-	int saldo;
-	pthread_mutex_lock(account_mutexes + (dualshock.idConta - 1));
-
-	switch(dualshock.operacao){
-
-		case OPERACAO_DEBITAR:
-
-			if (debitar (dualshock.idConta, dualshock.valor) < 0)
-				printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, dualshock.idConta, dualshock.valor);
-			else
-				printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, dualshock.idConta, dualshock.valor);
-
-			break;
-
-		case OPERACAO_CREDITAR:
-
-			if (creditar (dualshock.idConta, dualshock.valor) < 0)
-				printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, dualshock.idConta, dualshock.valor);
-			else
-				printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, dualshock.idConta, dualshock.valor);
-
-			break;
-
-		case OPERACAO_LER_SALDO:
-
-			saldo = lerSaldo(dualshock.idConta);
-
-			if (saldo < 0)
-				printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, dualshock.idConta);
-			else
-				printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, dualshock.idConta, saldo);
-
-			break;
-
-		case OPERACAO_SAIR:
-			flag_sair = 1;
-			break;
-
-		default:
-			printf("Comando Desconhecido.\n");
-			break;
-	}
-	
-	pthread_mutex_unlock(account_mutexes + (dualshock.idConta - 1));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-void processInput(){
-
-	buff_write_idx = 0;
-	buff_read_idx = 0;
-	flag_sair = 0;
-
-	char *args[MAXARGS + 1];
-	char buffer[BUFFER_SIZE]; 
-
-	int process_counter = 0; 
-	int pid_vector[NR_MAX_PROCESSOS];
-
-	while (1) {
-		int numargs;
-		numargs = readLineArguments(args, MAXARGS+1, buffer, BUFFER_SIZE);
-
-
-		/* 
-		 * Sair / Sair agora
-		 * termina o programa apos todos os comandos  no cmd_buffer
-		 * sejam concluidos pelos threads e que estes tenham terminado
-		 *
-		 * no caso de EOF (end of file) do stdin ou COMANDO_SAIR
-		 * arg1(optional) char* COMANDO_SAIR_AGORA
-		 * com arg1 manda os processos filhos terminarem
-		 * sem arg1 COMANDO_SAIR_AGORA espera que os filhos terminem  
-		 */
-		if ( numargs < 0 ||
-			(numargs > 0 && 
-			(strcmp(args[0], COMANDO_SAIR) == 0) )){
-			
-			int i;
-			int status;
-			int pid;
-			comando_t dualshock;
-
-			if (numargs > 2 || numargs < 1) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_SAIR);
-				continue;
-			}
-
-			printf("O i-banco vai terminar\n");
-			printf("--\n");
-
-			dualshock.operacao = OPERACAO_SAIR;
-			for ( i = 0; i < NUM_TRABALHADORAS ; i++)
-				addToBuffer(dualshock);
-
-
-			if (numargs == 2 && (strcmp(args[1], COMANDO_SAIR_AGORA) == 0) ){	
-				for(i = 0; i < process_counter; i++)
-					kill(pid_vector[i], SIGUSR1);
-			} 
-
-			
-
-			for (i = 0; i < process_counter; i++){
-				pid = wait (&status);
-				
-				if (WIFEXITED(status))
-					printf("FILHO TERMINADO (PID=%d; terminou normalmente)\n", pid);
-
-				else 
-					printf("FILHO TERMINADO (PID=%d; terminou abruptamente)\n", pid);
-			}
-			
-			
-			for ( i = 0; i < NUM_TRABALHADORAS ; i++){
-				pthread_join(thread_pool[i], NULL);
-			}
-
-			printf("--\n");
-			printf("O i-banco terminou\n");
-			exit(EXIT_SUCCESS);
-		}
-	
-
-
-		else if (numargs == 0)
-			/* Nenhum argumento; ignora e volta a pedir */
-			continue;
-	
-
-		/* 
-		 * Debitar 
-		 * arg 1 int - idConta
-		 * arg 2 int - valor
-		 * adiciona a ao cmd_buffer a OPERACAO_DEBITAR  
-		 */
-		else if (strcmp(args[0], COMANDO_DEBITAR) == 0) {
-			if (numargs < 3) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_DEBITAR);
-			   continue;
-			}
-
-			comando_t dualshock;
-
-			dualshock.operacao = OPERACAO_DEBITAR;
-			dualshock.idConta = atoi(args[1]);
-			dualshock.valor = atoi(args[2]);
-
-			addToBuffer(dualshock);
-
-		}
-
-		/*
-		 * Creditar 
-		 * arg 1 int - idConta
-		 * arg 2 int - valor
-		 * adiciona ao cmd_buffer a OPERACAO_CREDITAR
-		 */
-		else if (strcmp(args[0], COMANDO_CREDITAR) == 0) {
-			if (numargs < 3) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_CREDITAR);
-				continue;
-			}
-
-			comando_t dualshock;
-
-			dualshock.operacao = OPERACAO_CREDITAR;
-			dualshock.idConta = atoi(args[1]);
-			dualshock.valor = atoi(args[2]);
-
-			addToBuffer(dualshock);
-		}
-
-
-		/*
-		 * Ler Saldo
-		 * arg 1 int - idConta
-		 * adicionar ao cmd_buffer a OPERACAO_LER_SALDO
-		 */
-		else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
-			if (numargs < 2) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
-				continue;
-			}
-			
-			comando_t dualshock;
-
-			dualshock.operacao = OPERACAO_LER_SALDO;
-			dualshock.idConta = atoi(args[1]);
-						
-			addToBuffer(dualshock);
-		}
-
-		
-
-
-		/* 
-		 * Simular 
-		 * arg 1 int - nr_de_anos
-		 * faz a simulacao dos nr_de_anos sob as contas existentes
-		 */
-		else if (strcmp(args[0], COMANDO_SIMULAR) == 0) {
-			
-			if (numargs != 2 || atoi(args[1]) < 0) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_SIMULAR);
-				continue;
-			}
-
-			comando_t dualshock;
-
-			dualshock.operacao = OPERACAO_SIMULAR;
-
-			addToBuffer(dualshock);
-			
-			int pid = fork();
-
-			/* code for child process */
-			if (pid == 0){
-				simular(atoi(args[1]));
-				exit(EXIT_SUCCESS);
-			}
-
-			pid_vector[process_counter] = pid;
-			process_counter++;
-		}
-
-
-		else {
-			printf("Comando desconhecido. Tente de novo.\n");
-		}
-	}
-}
 
 
 int main (int argc, char** argv) {
 
 	int i;
+	int status;
 
+	signal(SIGUSR1, apanhaSinalSIGUSR1);
+	
+
+	/* Initializations */
 	inicializarContas();
-	signal(SIGUSR1, apanhaSinalSIGUSR1);
 
-	sem_init(&writer_sem, 0, CMD_BUFFER_DIM);
-	sem_init(&reader_sem, 0, 0);
+	/* initialize reading and writting semaphore writer_sem reading_sem*/
+	if( sem_init(&writer_sem, 0, CMD_BUFFER_DIM) != 0 ||
+		sem_init(&reader_sem, 0, 0) != 0){
+		printf("Erro a inicializar semaforos, i-banco vai terminar.\n");
+		exit(EXIT_FAILURE);
+	}
 
-	pthread_mutex_init(&reading_mutex, NULL);
+	/*initialize reading_mutex*/
+	if ( pthread_mutex_init(&reading_mutex, NULL) != 0){
+		printf("Erro a criar semaforos, i-banco vai terminar.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	/* initialize mutex vector account_mutexes*/
+	for(i = 0; i < NUM_CONTAS; i++){
+		if ( pthread_mutex_init(account_mutexes + i, NULL) != 0){
+			printf("Erro a inicializar mutexes, i-banco vai terminar.\n");
+			exit(EXIT_FAILURE);			
+		}
+	}
 
-	initAccountMutexes();
+	/* initialize thread_pool*/
+	for(i = 0; i < NUM_TRABALHADORAS; i++){
+		status = pthread_create(thread_pool + i, NULL, &readBuffer, NULL);
+		if (status != 0){
+			printf("Erro a criar threads, i-banco vai terminar.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-	for(i = 0; i < NUM_TRABALHADORAS; i++)
-		pthread_create(thread_pool + i, NULL, &readBuffer, NULL);
 
+
+	/* i-banco start */
 	printf("Bem-vinda/o ao i-banco\n\n");
+
 	processInput();
-}
 
-
-
-void apanhaSinalSIGUSR1(){
-	signal(SIGUSR1, apanhaSinalSIGUSR1);
-	flag = 1;
+	return 0;
 }
---
+++ exercicio2/inputProcessing.h
@@ -0,0 +1,120 @@
+
+/* 
+ * * * * * * * * * * * * *
+ * Grupo: 4
+ * Andre' Fonseca 84698
+ * Isabel Dias 84726
+ * 
+ * * * * * * * * * * * * *
+// Projeto SO - exercicio 2, version 1
+// Sistemas Operativos, DEI/IST/ULisboa 2016-17
+*/
+
+#include "commandlinereader.h"
+#include "contas.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+/* commands */
+#define COMANDO_DEBITAR "debitar"
+#define COMANDO_CREDITAR "creditar"
+#define COMANDO_LER_SALDO "lerSaldo"
+#define COMANDO_SIMULAR "simular"
+#define COMANDO_SAIR "sair"
+#define COMANDO_SAIR_AGORA "agora"
+
+/* operations related to the commands */
+#define OPERACAO_DEBITAR 1337
+#define OPERACAO_CREDITAR 666
+#define OPERACAO_LER_SALDO 007
+#define OPERACAO_SIMULAR 2319
+#define OPERACAO_SAIR 112
+
+/* temporary input buffer related defines*/ 
+#define MAXARGS 3
+#define BUFFER_SIZE 100
+
+/* threads and command related defines */ 
+#define NR_MAX_PROCESSOS 20
+#define NUM_TRABALHADORAS 3
+#define CMD_BUFFER_DIM (NUM_TRABALHADORAS * 2)
+
+
+/* command structure */
+typedef struct {
+	int operacao;
+	int idConta;
+	int valor;
+}comando_t;
+
+
+/* buffer with all given commands produced by input and consumed by threads */
+extern comando_t cmd_buffer[CMD_BUFFER_DIM];
+
+/* vector with all our threads aka pool*/
+extern pthread_t thread_pool[NUM_TRABALHADORAS];
+
+/* mutex to limit the access of the cmd buffer to 1 thread */
+extern pthread_mutex_t reading_mutex;
+
+/* mutex vector to limit the access to a single thread per account*/
+extern pthread_mutex_t account_mutexes[NUM_CONTAS];
+
+/* semaphore that controls the insertion of new commands on cmd buffer */
+extern sem_t writer_sem;
+
+/* semaphore that controls the consuption of new commands on cmd buffer */
+extern sem_t reader_sem;
+
+/* tracks cmd buffer's next writting position*/
+extern int buff_write_idx;
+
+/* tracks cmd buffer's next reading position */
+extern int buff_read_idx;
+
+
+
+void apanhaSinalSIGUSR1();
+/** 
+ * function that processes signal SIGUSR1 on child processes
+ */
+
+
+
+void addToBuffer(comando_t comando);
+/**
+ * adds to cmd buffer the given comando-t (command) if 
+ * there is room in buffer for it, else it waits until 
+ * it is able to add to buffer
+ * @param comando command to be written on cmd buffer
+ */
+
+void* readBuffer();
+/**
+ * Continuously reads and processes commands from buffer 
+ * until a specific command is given to stop.
+ *
+ * Used by threads to consume and process from the buffer 
+ * while there are commands left to be processed.
+ */
+
+void processCommand(comando_t comando);
+/**
+ * Processes/executes the given command inside in a way
+ * that limits one thread per account ID
+ * 
+ * arg: comand_t comando
+ */
+
+void processInput();
+/**
+ * continuously asks for input until stop command is given
+ * converts the program's input into commands (comando_t)
+ * verifies basic command realted info before creating command
+ */
